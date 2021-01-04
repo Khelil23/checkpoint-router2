4bc3039afe1e63cb1c03e58fbc4e967d10678f07
khelil23 <khelil-23@outlook.fr>
2021-01-04 15:06:16 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import Home from './components/Home';
+import  Trailer from './components/Trailer';
+import {BrowserRouter as Router,Route,Switch,Link} from 'react-router-dom'
+
+import React from 'react';
+
+ 
+
+function App() {
+  
+  return (
+    <div className="App">
+       <>
+  <Router>
+   
+  <Switch>
+    <Route exact path='/' component={Home}/>
+    <Route path="/trailer/:moviesId" component={Trailer} />
+  </Switch>
+  </Router>
+  </>
+    </div>
+  );
+}
+
+export default App;
